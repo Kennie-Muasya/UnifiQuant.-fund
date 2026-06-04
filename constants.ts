2c904c35358ec584681cb25906beb25faceba855
Kennie-Muasya <kennmuasya49@gmail.com>
2026-06-04 13:40:36 +0300

    Add statistics constant with key metrics

---
+++ constants.ts
@@ -0,0 +1,18 @@
+export const statistics = [
+  {
+    value: "12.4M+",
+    label: "Backtests Processed"
+  },
+  {
+    value: "45K+",
+    label: "Global Instruments"
+  },
+  {
+    value: "820+",
+    label: "Research Models"
+  },
+  {
+    value: "24/7",
+    label: "AI Infrastructure"
+  }
+];
