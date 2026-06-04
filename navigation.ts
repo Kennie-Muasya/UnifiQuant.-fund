e0338b70d97af3c13ec3af140b6d46d845e3bdce
Kennie-Muasya <kennmuasya49@gmail.com>
2026-06-04 13:39:29 +0300

    Add navigation array with route names and links

---
+++ navigation.ts
@@ -0,0 +1,26 @@
+export const navigation = [
+  {
+    name: "Home",
+    href: "/"
+  },
+  {
+    name: "Research",
+    href: "/research"
+  },
+  {
+    name: "Strategies",
+    href: "/strategies"
+  },
+  {
+    name: "Technology",
+    href: "/technology"
+  },
+  {
+    name: "Analytics",
+    href: "/analytics"
+  },
+  {
+    name: "Dashboard",
+    href: "/dashboard"
+  }
+];
